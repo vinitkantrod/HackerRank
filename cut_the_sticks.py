af8edc96b8743dc6620dffb76754bcac819720f3
noop <noopur.nawge@zettamine.com>
2015-04-09 22:45:35 +0530

    9th

--- cut_the_sticks.py
+++ cut_the_sticks.py
@@ -7,16 +7,19 @@ while(len(arr)!=0):
     m = min(arr)
     #print m
     cou=0
-    for i in range(0,len(arr)):
+    for i in range(0,(len(arr))):
         c = arr[i]-m
+        cou = cou+1
         #print c
         if(c!=0):
            arr1.append(c)
-           cou =cou + 1
-           count.append(cou)
+           print cou
+           #count.append(cou)
         else:
             print arr[i],'deleted'
             del arr[i]
+    count.append(cou)
     arr = arr1
-for k in count:
-    print k
+#for k in count:
+    #print k
+print(' '.join((str(e)) for e in count))
