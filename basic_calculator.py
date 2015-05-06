37aab10fca122639e344790c0d5825935ea32e69
Vinit kantrod <kantrod.vinit2013@vit.ac.in>
2015-05-06 12:48:06 +0530

    comited 6th may

---
+++ basic_calculator.py
@@ -0,0 +1,12 @@
+a = float(input())
+b = float(input())
+add = a+b
+print("%0.2f"%(add))
+sub = a-b
+print("%0.2f"%(sub))
+mul = a*b
+print("%0.2f"%(mul))
+div = a/b
+print("%0.2f"%(div))
+int_div = a//b
+print("%0.2f"%(int_div))
